cebe4f9dca962258dd35daf5fa41395d5ab5ff70
George Dewar <george@dewar.co.nz>
2014-09-06 17:05:46 +1200

    Support AC and Switch

--- app/models/device.rb
+++ app/models/device.rb
@@ -3,11 +3,30 @@ class Device < ActiveRecord::Base
   has_many :messages
 
   def last_state
-    messages.last ? JSON.parse(Message.last.message, symbolize_names: true) : FujitsuAC.defaults
+    messages.last ? JSON.parse(messages.last.message, symbolize_names: true) : defaults
+  end
+
+  def defaults
+    return FujitsuAC.defaults if definition == 'FujitsuAC'
+    return {:state => :on} if definition == 'WattsClever'
+    raise "Unknown definition: #{definition}"
+  end
+
+  def encode_message(message)
+    puts message.inspect
+    puts self.inspect
+    case definition
+      when 'FujitsuAC'
+        return 'IF' + FujitsuAC.generate(message)
+      when 'WattsClever'
+        return 'RS' + JSON.parse(properties, symbolize_names: true)[message[:state].to_sym]
+      else
+        raise "Unknown definition: #{definition}"
+    end
   end
 
   def attributes
-    attributes = {
+    fuj_attributes = {
         :temp => OpenStruct.new({
             name: 'Temperature',
             type: :range,
@@ -32,7 +51,17 @@ class Device < ActiveRecord::Base
             options: [:on, :off]
         })
     }
-    attributes
+    sw_attributes = {
+        :state => OpenStruct.new({
+           name: 'State',
+           type: :discrete,
+           role: :command,
+           horizontal: true,
+           options: [:on, :off]
+        })
+    }
+
+    definition == 'FujitsuAC' ? fuj_attributes : sw_attributes
   end
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
   belongs_to :device
 
   def to_raw
-    'IF' + FujitsuAC.generate(JSON.parse(message, symbolize_names: true))
+    device.encode_message(JSON.parse(message, symbolize_names: true))
   end
 
 end
