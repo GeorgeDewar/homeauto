ab512f6cf9653046b4db40e110cc2e3352ce54c3
George Dewar <george@dewar.co.nz>
2014-09-06 12:06:04 +1200

    IF (for Infrared -> Fujitsu) not IR

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
   belongs_to :device
 
   def to_raw
-    'IR' + FujitsuAC.generate(JSON.parse(message, symbolize_names: true))
+    'IF' + FujitsuAC.generate(JSON.parse(message, symbolize_names: true))
   end
 
 end
